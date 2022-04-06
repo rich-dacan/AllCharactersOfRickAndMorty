c9658352cac83369eccc0df8290cc2202a1a1a83
Richard Cândido <rich_dacan@protonmail.com>
2022-04-06 20:52:47 -0300

    create: ALL characters of Rick and Morty!

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import Characters   from './components/Characters';
+
+function App() {
+
+  const [characterList, setCharacterList] = useState([]);
+
+  const [pageInfo, setPageInfo]           = useState([]);
+
+  const [currentPage, setCurrentPage]     = useState('https://rickandmortyapi.com/api/character/');
+
+  useEffect(() => {
+
+    fetch(currentPage) 
+
+      .then((response) => response.json())
+      .then((response) => {
+
+        setCharacterList(response.results)
+        setPageInfo(response.info)
+
+      })
+
+      .catch((err)     => console.log(err));
+      
+    }, [currentPage, ]);
+
+
+    console.log(currentPage);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <Characters listCharacters={characterList} />
+
+        <button className='next__page' onClick={() => setCurrentPage(pageInfo.next)}>Next Page</button>
+        <button className='next__page' onClick={() => setCurrentPage(pageInfo.prev)}>Prev Page</button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CharCard/index.jsx
@@ -0,0 +1,69 @@
+import './styles.css';
+
+const CharCard = ({name, image, status, species, gender, type, origin, location}) => {
+  
+  return (
+
+
+    <>
+      {status === 'Dead' ?
+
+        <div className="flip-container" >
+          <div className="flipper" >
+            <div className='front'>
+              <li>
+                <span className='card__container'>
+
+                  <img className='dead' src={image} alt="character"  />
+                  <p className='name__person'>{name}</p>
+
+                </span>
+              </li>
+            </div>
+            <div className='back'>
+              <h4 className='dead'>Status of life: {status}</h4>
+              <p><b>Species:</b> {species}</p>
+              <p><b>Gender:</b> {gender}</p>
+              <p><b>Type:</b> {type}</p>
+              <p><b>Origin:</b> {origin}</p>
+              <p><b>Location:</b> {location}</p>
+
+            </div>
+          </div>
+        </div>
+      
+        
+      : 
+
+      <div className="flip-container" >
+          <div className="flipper" >
+            <div className='front'>
+              <li>
+                <span className='card__container'>
+
+                  <img src={image} alt="character"  />
+                  <p className='name__person'>{name}</p>
+
+                </span>
+              </li>
+            </div>
+            <div className='back'>
+              <h4 className='alive'>Status of life: {status}</h4>
+              <p><b>Species:</b> {species}</p>
+              <p><b>Gender:</b> {gender}</p>
+              <p><b>Type:</b> {type}</p>
+              <p><b>Origin:</b> {origin}</p>
+              <p><b>Location:</b> {location}</p>
+
+            </div>
+          </div>
+        </div>
+        
+      
+      }
+    
+    </>
+  )
+}
+
+export default CharCard;
