20ce4a0a020927a543dfcb78bc5c904069771305
Richard Cândido <rich_dacan@protonmail.com>
2023-06-08 09:14:41 -0300

    fix :bug: fixed lint

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import Characters   from './components/Characters';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
-
   const [characterList, setCharacterList] = useState([]);
 
-  const [pageInfo, setPageInfo]           = useState([]);
+  const [pageInfo, setPageInfo] = useState([]);
 
-  const [currentPage, setCurrentPage]     = useState('https://rickandmortyapi.com/api/character/');
+  const [currentPage, setCurrentPage] = useState(
+    "https://rickandmortyapi.com/api/character/"
+  );
 
   useEffect(() => {
-
-    fetch(currentPage) 
-
-      .then((response) => response.json())
-      .then((response) => {
-
-        setCharacterList(response.results)
-        setPageInfo(response.info)
-
+    fetch(currentPage)
+      .then(response => response.json())
+      .then(response => {
+        setCharacterList(response.results);
+        setPageInfo(response.info);
       })
 
-      .catch((err)     => console.log(err));
-      
-    }, [currentPage, ]);
+      .catch(err => console.log(err));
+  }, [currentPage]);
 
-
-    console.log(currentPage);
+  console.log(currentPage);
 
   return (
     <div className="App">
       <header className="App-header">
-
         <Characters listCharacters={characterList} />
 
-        <button className='next__page' onClick={() => setCurrentPage(pageInfo.next)}>Next Page</button>
-        <button className='next__page' onClick={() => setCurrentPage(pageInfo.prev)}>Prev Page</button>
-
+        <button
+          className="next__page"
+          onClick={() => setCurrentPage(pageInfo.next)}
+        >
+          Next Page
+        </button>
+        <button
+          className="next__page"
+          onClick={() => setCurrentPage(pageInfo.prev)}
+        >
+          Prev Page
+        </button>
       </header>
     </div>
   );
--- src/components/Characters/index.jsx
+++ src/components/Characters/index.jsx
@@ -1,27 +1,45 @@
-import './styles.css';
-import CharCard from '../CharCard';
+import "./styles.css";
+import CharCard from "../CharCard";
 
-const Characters = ({listCharacters}) => {
-
-  // console.log(listCharacters)
-
+const Characters = ({ listCharacters }) => {
   return (
     <>
       <h1>Characters from </h1>
-      <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg" alt="Open - Rick And Morty Svg Free Clipart@pikpng.com"/>
-      
-      <ul >
+      <img
+        className="logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg"
+        alt="Open - Rick And Morty Svg Free Clipart@pikpng.com"
+      />
 
-        {listCharacters.map(({name, image, id, status, species, gender, type, origin, location }) => (
-
-          <CharCard name={name} image={image} key={id} status={status} species={species} gender={gender} type={type} origin={origin.name} location={location.name}/>
-
-        ))}
-
+      <ul>
+        {listCharacters.map(
+          ({
+            name,
+            image,
+            id,
+            status,
+            species,
+            gender,
+            type,
+            origin,
+            location,
+          }) => (
+            <CharCard
+              name={name}
+              image={image}
+              key={id}
+              status={status}
+              species={species}
+              gender={gender}
+              type={type}
+              origin={origin.name}
+              location={location.name}
+            />
+          )
+        )}
       </ul>
-    
     </>
-  )
-}
+  );
+};
 
 export default Characters;
